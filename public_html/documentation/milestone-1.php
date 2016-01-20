338d3964e40cbc83cf829328eb26075600a7a888
zleyba <mtvzach@gmail.com>
2016-01-19 23:30:32 -0700

    config and initial html files

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Kellehfish Milestone 1</title>
+	</head>
+	<body>
+		<header>
+			<h1>K E L L E H F I S H</h1>
+			<h2>design.doc</h2>
+		</header>
+		<div class="content">
+
+			<h3>p u r p o s e</h3>
+
+			<p>
+				The purpose of this website is to assist Kelly Williams in the process of
+			finding new clients and collaborators and will serve as a reference point
+			for galleries and other organizations who are interested in seeing and possibly
+			showing her work. </p>
+
+			<br>
+
+			<h3>a u d i e n c e</h3>
+
+			<p>
+				The primary users this website consist of potential collaborators who are
+				primarly artists and arts org reps between the ages of 24-64.
+			</p>
+			<br>
+			<h3>g o a l s</h3>
+			<p>To facilitate opportunities for Kelly's art career. Potentially one
+			day serve as a way to sell or drive sales of Kelly's art.</p>
+			<br>
+
+		</div>
+		<div class="persona">
+
+			<div class="profile">
+				<img src="img/allix.jpg">
+				<h3>Allix Cohen</h3>
+				<h4>“I’m so excited to get the ball rolling with the upcoming local-women's showcase!”</h4>
+			</div>
+
+			<div ="descript">
+			<p> Allix is a goal oriented and task-minded. She wants to iron out the big concrete details quickly so she
+			can handle the little details where her skillset shines the brightest. She’s less concerned about putting her
+			personal stamp on the event she’s throwing and would rather bring in artists who are easy to work with.</p>
+		</div>
+
+		</div>
+	</body>
+</html>
