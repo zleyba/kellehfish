f71049cddc1056a35c815725c0b5db2ddbeccbd5
zleyba <mtvzach@gmail.com>
2016-01-27 23:32:34 -0700

    initial php tests

---
+++ public_html/documentation/bootstrapwork.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<?include_once ("../php/bootstrap_load.html")?>
+		<link type="text/css" href="../css/style.css" rel="stylesheet" />
+	</head>
+	<body>
+		<div class="container">
+
+			<div class="row">
+				<div class="col-md-4"> is this thing on</div>
+				<div class="col-md-4"> </div>
+				<div class="col-md-4"> when will it bee</div>
+			</div>
+	</body>
+</html>
