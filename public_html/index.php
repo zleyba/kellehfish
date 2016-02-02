bc9e59834d26e0f9c75a91afb4614848d11781c7
zleyba <mtvzach@gmail.com>
2016-02-01 22:34:03 -0700

    adding css and index file

---
+++ public_html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+ * Created by PhpStorm.
+ * User: zleyba
+ * Date: 2/1/16
+ * Time: 10:24 PM
+ -->
+	<html>
+	<head>
+		<meta charset="UTF-8">
+
+		<link type="text/css" href="../css/kfcss.css" rel="stylesheet" />
+
+	</head>
+</html>
