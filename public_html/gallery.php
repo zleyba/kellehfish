4072452c9c1252f87d88c545f4d6f231a1a0c638
zleyba <mtvzach@gmail.com>
2016-02-02 11:07:24 -0700

    building out gallery

---
+++ public_html/gallery.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<!--
+ * Created by PhpStorm.
+ * User: zleyba
+* Date: 2/2/16
+* Time: 10:24 AM
+-->
+	<html>
+	<head>
+		<meta charset="UTF-8">
+		<?php require_once(dirname(__DIR__) . "/public_html/lib/head-tags.html"); ?>
+<link href='https://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Didot' rel='stylesheet' type='text/css'>
+		<!-- loads gallery support files-->
+		<link rel="stylesheet" href="../public_html/css/swipebox.css">
+		<script src="../public_html/lib/jquery-2.1.0.min.js"></script>
+		<script src="../public_html/js/jquery.swipebox.js"></script>
+		<script type="text/javascript">
+			;( function( $ ) {
+
+				$( '.swipebox' ).swipebox();
+
+			} )( jQuery );
+		</script>
+		<!--end gallery support-->
+
+<link type="text/css" href="../public_html/css/kfcss.css" rel="stylesheet" />
+<title>The Personal Website of Illustrator Kelly Williams</title>
+</head>
+<body>
+	<header>
+		<div class="sfooter container">
+			<h1>kellehfish</h1>
+			<hr>
+			<nav>
+				<div class="row">
+					<div class="col-md-2">
+						<ul>
+							<li><a class="iglogo" href="https://www.instagram.com/kellehfish/"></a></li>
+							<li><a class="tumblrlogo" href="http://kellehjelleh.tumblr.com/"></a> </li>
+						</ul>
+
+					</div>
+
+					<div class="col-md-10">
+						<ul>
+							<li><a class="btn btn-default" href="#" role="button">h o m e</a></li>
+							<li><a class="btn btn-default" href="#" role="button">w o r k</a></li>
+							<li><a class="btn btn-default" href="#" role="button">a b o u t</a></li>
+							<li><a class="btn btn-default" href="#" role="button">c o n t a c t</a></li>
+						</ul>
+					</div>
+
+				</div>
+			</nav>
+
+		</div>
+
+	</header>
+
+
+	<div class="sfooter-content container">
+		<div class="wrap small-width">
+			<div id="try"></div>
+			<ul id="box-container">
+				<li class="box">
+					<a href="http://swipebox.csag.co/images/image-1.jpg" class="swipebox" title="Fog">
+						<img src="http://swipebox.csag.co/images/image-1.jpg" alt="image">
+					</a>
+				</li>
+				<li class="box">
+					<a href="http://swipebox.csag.co/images/image-2.jpg" class="swipebox" title="City">
+						<img src="http://swipebox.csag.co/images/image-2.jpg" alt="image">
+					</a>
+				</li>
+				<li class="box">
+					<a href="http://swipebox.csag.co/images/image-3.jpg" class="swipebox" title="Street">
+						<img src="http://swipebox.csag.co/images/image-3.jpg" alt="image">
+					</a>
+				</li>
+				<li class="box">
+					<a href="http://swipebox.csag.co/images/image-4.jpg" class="swipebox" title="Mustache Guy">
+						<img src="http://swipebox.csag.co/images/image-4.jpg" alt="image">
+					</a>
+				</li>
+			</ul>
+		</div>
+
+	</div>
+
+
+	<footer>
+		(c) Kelly Williams 2016
+	</footer>
+
+
+
+</body>
+</html>
