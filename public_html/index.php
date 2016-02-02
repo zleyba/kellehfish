751e251b179608e2c99412d2a91d2882d13f9228
zleyba <mtvzach@gmail.com>
2016-02-02 09:17:40 -0700

    configured s-footer

--- public_html/index.php
+++ public_html/index.php
@@ -10,28 +10,49 @@
 		<meta charset="UTF-8">
 		<?php require_once(dirname(__DIR__) . "/public_html/lib/head-tags.html"); ?>
 		<link href='https://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Didot' rel='stylesheet' type='text/css'>
 		<link type="text/css" href="../public_html/css/kfcss.css" rel="stylesheet" />
 		<title>The Personal Website of Illustrator Kelly Williams</title>
 	</head>
 	<body>
-		<header>
+		<header class="sfooter">
 			<div class="container">
-				<h1>KELLEHFISH</h1>
+				<h1>kellehfish</h1>
 				<hr>
 				<nav>
-					<ul>
-						<li><a class="iglogo" href="#">IG</a></li>
-					<ul>
-						<li><a class="btn btn-default" href="#" role="button">Home</a></li>
-						<li><a class="btn btn-default" href="#" role="button">Work</a></li>
-						<li><a class="btn btn-default" href="#" role="button">About</a></li>
-						<li><a class="btn btn-default" href="#" role="button">Contact</a></li>
-					</ul>
+					<div class="row">
+					<div class="col-md-2">
+						<ul>
+							<li><a class="iglogo" href="https://www.instagram.com/kellehfish/"></a></li>
+							<li><a class="tumblrlogo" href="http://kellehjelleh.tumblr.com/"></a> </li>
+						</ul>
+
+					</div>
+
+						<div class="col-md-10">
+							<ul>
+								<li><a class="btn btn-default" href="#" role="button">h o m e</a></li>
+								<li><a class="btn btn-default" href="#" role="button">w o r k</a></li>
+								<li><a class="btn btn-default" href="#" role="button">a b o u t</a></li>
+								<li><a class="btn btn-default" href="#" role="button">c o n t a c t</a></li>
+							</ul>
+					</div>
+
+					</div>
 				</nav>
 
 			</div>
 
 		</header>
+
+		<div class="sfooter-content">
+
+
+		</div>
+
+		<footer>
+			(c) Kelly Williams 2016
+		</footer>
 
 
 
