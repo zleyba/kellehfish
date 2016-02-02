8e7233626861d36aeb9ebf2bb4ce9755758b931c
zleyba <mtvzach@gmail.com>
2016-02-01 23:55:18 -0700

    began work on website

--- public_html/index.php
+++ public_html/index.php
@@ -9,11 +9,31 @@
 	<head>
 		<meta charset="UTF-8">
 		<?php require_once(dirname(__DIR__) . "/public_html/lib/head-tags.html"); ?>
+		<link href='https://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
 		<link type="text/css" href="../public_html/css/kfcss.css" rel="stylesheet" />
 		<title>The Personal Website of Illustrator Kelly Williams</title>
 	</head>
 	<body>
+		<header>
+			<div class="container">
+				<h1>KELLEHFISH</h1>
+				<hr>
+				<nav>
+					<ul>
+						<li><a class="iglogo" href="#"><img src="../public_html/img/iglogo.png"></a></li>
+					<ul>
+						<li><a class="btn btn-default" href="#" role="button">Home</a></li>
+						<li><a class="btn btn-default" href="#" role="button">Work</a></li>
+						<li><a class="btn btn-default" href="#" role="button">About</a></li>
+						<li><a class="btn btn-default" href="#" role="button">Contact</a></li>
+					</ul>
+				</nav>
 
-		<h1>KELLEHFISH</h1>
+			</div>
+
+		</header>
+
+
+
 	</body>
 </html>
